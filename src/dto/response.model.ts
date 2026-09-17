f51d1dc5d2a6ddd24d6c1981af09dc85765230ec
ANDERSON DOS SANTOS BARBOSA <51709894+AndersonSBarbosa@users.noreply.github.com>
2026-09-17 17:55:57 -0300

    Delete src directory

--- src/dto/response.model.ts
+++
@@ -1,7 +0,0 @@
-// src/dto/response.model.ts
-
-export interface ResponseModel<T> {
-  success: boolean;
-  message?: string;
-  data?: T;
-}
--- src/errors/app.error.ts
+++
@@ -1,9 +0,0 @@
-// src/errors/app.error.ts
-export class AppError extends Error {
-  constructor(
-    public message: string,
-    public statusCode = 500
-  ) {
-    super(message);
-  }
-}
